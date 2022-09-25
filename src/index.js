310cba10af5423fca670e9f36228e592692cd802
MejanH <muhammad.mejanul.haque@gmail.com>
2022-09-25 21:02:30 +0600

    feat: changed mysql npm package for v8 authentication

--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
-const http = require('http');
+const http = require("http");
+
+var mysql = require("mysql2");
+var connection = mysql.createConnection({
+  host: "database",
+  user: "root",
+  password: "password",
+  port: 3306,
+});
+
+connection.query("CREATE DATABASE IF NOT EXISTS testdb", function (err, result) {
+  if (err) throw err;
+  console.log("database created");
+});
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+
+//   console.log("connected as id " + connection.threadId);
+// });
 
 const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    switch (req.url) {
-        case "/":
-            res.end(JSON.stringify([
-                {
-                    title: "meeting with john doe",
-                    completed: false
-                },
-                {
-                    title: "buy a keyboard",
-                    completed: false
-                }
-            ]));
-            break;
+  res.setHeader("Content-Type", "application/json");
+  switch (req.url) {
+    case "/":
+      res.end(
+        JSON.stringify([
+          {
+            title: "meeting with john doe",
+            completed: false,
+          },
+          {
+            title: "buy a keyboard",
+            completed: false,
+          },
+        ])
+      );
+      break;
 
-        default:
-            res.end("no data")
-            break;
-    }
-}
+    default:
+      res.end("no data");
+      break;
+  }
+};
 
 const server = http.createServer(requestListener);
 server.listen(8080, undefined, () => {
-    console.log(`Server running on http://localhost:8080`)
+  console.log(`Server running on http://localhost:8080`);
 });
